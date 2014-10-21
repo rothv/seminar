0d00ac8730eec8c0c17ae35181e7278ac12da98d
Mihail Atanassov <atanassov.mihail@gmail.com>
2014-10-21 23:14:44 +0200

    Add second homework project including syntax errors.

---
+++ Aufgabe 2/src/Application.java
@@ -0,0 +1,16 @@
+
+public class MeinProgramm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Fehler2 fehler = new Fehler2();
+		fehler2.addValues(1,2);
+		fehler2.printString("Dieser String wird 5 mal ausgegeben");
+		fehler2.printNumber(10, 12);
+		
+
+	}
+
+}
