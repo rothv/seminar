a98fa4d4942bb42b67addc2a9a765851a1334c98
rothv <rothv@smail.uni-koeln.de>
2014-10-24 23:44:10 +0200

    fertig

---
+++ Aufgabe2_Zusatz/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Katze kitty = new Katze();
+		
+		kitty.name("Kitty");
+		kitty.fell("schwarz");
+		kitty.alter(10);
+		kitty.weiblich(true);
+		
+		
+
+	}
+
+}
