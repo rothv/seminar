0f97fbbcb6903d76e657a61bad5865195b833808
rothv <rothv@smail.uni-koeln.de>
2014-10-24 23:43:20 +0200

    der zweite teil der hausaufgabe 22.10

---
+++ Aufgabe2_Zusatz/src/Katze.java
@@ -0,0 +1,22 @@
+
+public class Katze {
+	
+	void name (String name){
+		System.out.println("Die Katze heiﬂt " + name);
+	}
+
+	void alter (Integer alter){
+	System.out.println("Die Katze ist " + alter);
+	}
+	void fell(String fell){
+	System.out.println("Die Katze ist " +fell);
+	}
+	
+	void weiblich(Boolean weiblich){
+	System.out.println("Die Katze ist weiblich " + weiblich);
+	}
+	
+	void miau(){
+		System.out.println("Miau");
+	}
+}
