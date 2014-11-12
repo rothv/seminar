7a728c11ad1375c8f1508d5872f554df1215ec9c
rothv <rothv@smail.uni-koeln.de>
2014-11-12 13:03:15 +0100

    ha

---
+++ Intervall/src/App.java
@@ -0,0 +1,33 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Integer v = 1;
+		Integer b = 1;
+		
+		
+		
+		public Integer addiereVonBis(Integer von; Integer bis) {
+
+			for (Integer i =von;von==bis;i++){
+				i=von+(bis-1);
+				System.out.println("i");
+			}
+			
+
+			}
+
+	}
+
+}
+//Werte sind Integer
+
+//1von 1bis
+//1+1=2
+
+//2von 5bis
+//2+3+4+5=14
+//von+1 == bis
+//break
+//system.out.println(ergebnis)
